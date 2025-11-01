ce83dd99bc975b4244e6a18dd8d52f5569244753
Jintian-JTST <wangjintianlovemc@163.com>
2025-11-01 13:43:18 +0000

    Done qu.2 of pandas.

--- upload.bat
+++ upload.bat
@@ -2,7 +2,7 @@
 chcp 65001 >nul
 cd /d "C:\Users\JTST\OxfordTime"
 
-set /p msg=What did you update? Press Enter to escape:
+set /p msg=What did you update? Press Enter to escape: 
 
 if "%msg%"=="" (
     echo No commit, no update.
